ab4710fb7a73d0b61a9713fe2fbe198a699938bc
jditzelnew <58816213+jditzelnew@users.noreply.github.com>
2024-11-28 08:48:36 +0100

    Updating Copyrights

--- DataFormats/simulation/include/SimulationDataFormat/O2DatabasePDG.h
+++ DataFormats/simulation/include/SimulationDataFormat/O2DatabasePDG.h
@@ -1,6 +1,6 @@
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright
-// holders. All rights not expressly granted are reserved.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
 // This software is distributed under the terms of the GNU General Public
 // License v3 (GPL Version 3), copied verbatim in the file "COPYING".
@@ -8,10 +8,6 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-
-//
-// Created by Sandro Wenzel on 11.08.22.
-//
 
 #ifndef O2_O2DATABASEPDG_H
 #define O2_O2DATABASEPDG_H
